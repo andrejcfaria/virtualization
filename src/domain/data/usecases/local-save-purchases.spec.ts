28e1cd2ffad9a33eddfe55d8fa9c8d7a3aa3f44f
andre faria <fariaoandre@gmail.com>
2024-12-17 17:21:09 +0000

    test: ensure local save purchases deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -4,18 +4,33 @@ describe('LocalSave', () => {
     const sut = new LocalSavePurchases(cacheStore);
     expect(cacheStore.deleteCallsCount).toBe(0);
   })
+  test("Should  delete old cache on save",async () => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchases(cacheStore);
+  await  sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+  })
  })
 
 
  class LocalSavePurchases {
   constructor(private readonly cacheStore:CacheStore) {}
+
+  async save ():Promise<void> {
+    this.cacheStore.delete();
+  }
  }
 
 
 
   interface CacheStore {
+    delete ():void;
  }
 
   class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete () :void {
+    this.deleteCallsCount++
+  }
  }
