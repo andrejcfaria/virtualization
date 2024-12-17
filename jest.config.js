34575fa60cd1afb69a3342bfb964a3237d3f9aaf
andre faria <fariaoandre@gmail.com>
2024-12-17 16:52:10 +0000

    chore: jest cfg

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+
 module.exports = {
   roots: ["<rootDir>/src"],
   testEnviroment :' node',
   transform: {
     '.+\\.ts$' : 'ts-jest'
+  },
+  pathsToModuleNameMapper: {
+    '@/{.*}': '<rootDir>/src/$1'
   }
 }
