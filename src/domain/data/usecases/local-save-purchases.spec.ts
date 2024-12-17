ad00dbcff687c9569c4852ffb4fecd62fc6aa78b
andre faria <fariaoandre@gmail.com>
2024-12-17 17:39:18 +0000

    test: ensure cache is cleared with correct key

--- src/domain/data/usecases/local-save-purchases.spec.ts
+++ src/domain/data/usecases/local-save-purchases.spec.ts
@@ -8,6 +8,11 @@ const {sut, cacheStore} = makeSut()
     await  sut.save();
     expect(cacheStore.deleteCallsCount).toBe(1);
   })
+  test("Should call  delete with correct key",async () => {
+    const {sut, cacheStore} = makeSut()
+    await  sut.save();
+    expect(cacheStore.key).toBe("purchases");
+  })
  })
 
 
@@ -15,21 +20,23 @@ const {sut, cacheStore} = makeSut()
   constructor(private readonly cacheStore:CacheStore) {}
 
   async save ():Promise<void> {
-    this.cacheStore.delete();
+    this.cacheStore.delete("purchases");
   }
  }
 
 
 
   interface CacheStore {
-    delete ():void;
+    delete :(key:string)=>void;
  }
 
   class CacheStoreSpy implements CacheStore {
-  deleteCallsCount = 0;
+  deleteCallsCount = 0
+  key:string = ""
 
-  delete () :void {
+  delete (key:string) :void {
     this.deleteCallsCount++
+    this.key = key;
   }
  }
 
